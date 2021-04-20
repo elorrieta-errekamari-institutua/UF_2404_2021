efc6391a8137681280cb003cf45344febc444f49
Admin <Admin@HZ327394.elorrieta.int>
2021-04-20 09:06:26 +0200

    capturar Excepcion

--- src/com/elorrieta/objetos/CrearEmpleado.java
+++ src/com/elorrieta/objetos/CrearEmpleado.java
@@ -31,7 +31,12 @@ public class CrearEmpleado {
 		if (!becario) { // becario != true
 			do {
 				System.out.println("Dime el sueldo en euros ( minimo " + Empleado.SUELDO_MINIMO + ")");
-				sueldo = Float.parseFloat(sc.nextLine());
+				try {
+					sueldo = Float.parseFloat(sc.nextLine());
+				} catch (Exception e) {
+					System.out.println("Formato no correcto, por favor intentalo de nuevo");
+					// e.printStackTrace(); // pinta la traza de la Exception en pantalla
+				}
 			} while (sueldo < Empleado.SUELDO_MINIMO);
 		}
 
