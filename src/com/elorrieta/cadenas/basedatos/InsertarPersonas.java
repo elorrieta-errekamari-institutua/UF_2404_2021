cdcdc6622f0a51920fd748ad4bf07475d8f4fdef
Admin <Admin@HZ327394.elorrieta.int>
2021-04-23 12:46:56 +0200

    bucle para insertar las personas deseadas

--- src/com/elorrieta/cadenas/basedatos/InsertarPersonas.java
+++ src/com/elorrieta/cadenas/basedatos/InsertarPersonas.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class InsertarPersonas {
 
@@ -11,20 +12,36 @@ public class InsertarPersonas {
 
 		String sql = "INSERT INTO person (id, name) VALUES ( ?, ?) ;";
 
-		try (Connection con = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		try (Scanner sc = new Scanner(System.in);
+				Connection con = DriverManager.getConnection("jdbc:sqlite:sample.db");
 				PreparedStatement pst = con.prepareStatement(sql)) {
 
-			// TODO pedir datos por consola
-			String nombre = "Marian Antonela";
-			int id = 35;
+			String salir = "";
+			while (!"s".equalsIgnoreCase(salir)) {
 
-			// sustituimos las '?' de la SQL por las variables
-			pst.setInt(1, id);
-			pst.setString(2, nombre);
+				System.out.println("Pedir nombre");
+				String nombre = sc.nextLine();
 
-			pst.executeUpdate(); // ejecuta la SQL contra la bbdd que nos hemos conectado
+				// TODO pst.executeUpdate();
+				System.out.println("perrsona insertada");
 
-			System.out.println("Persona insertada");
+				System.out.println("¿ quieres salir ? S-Si     N-No");
+				salir = sc.nextLine();
+
+			}
+
+			/*
+			 * // TODO pedir datos por consola String nombre = "Marian Antonela"; int id =
+			 * 35;
+			 * 
+			 * // sustituimos las '?' de la SQL por las variables pst.setInt(1, id);
+			 * pst.setString(2, nombre);
+			 * 
+			 * pst.executeUpdate(); // ejecuta la SQL contra la bbdd que nos hemos conectado
+			 * 
+			 * System.out.println("Persona insertada");
+			 * 
+			 */
 
 		} catch (SQLException e) {
 			e.printStackTrace();
